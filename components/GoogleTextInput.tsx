22d361b471960fafd26b4ee3cc902cd6891ce77f
jhon varon <jhonvaronyate@hotmail.com>
2024-09-09 10:48:38 -0500

    complete HomePage UI and create RideCard component

---
+++ components/GoogleTextInput.tsx
@@ -0,0 +1,15 @@
+import { GoogleInputProps } from "@/types/type";
+import React from "react";
+import { Text } from "react-native";
+
+const GoogleTextInput = ({
+  icon,
+  initialLocation,
+  containerStyle,
+  textInputBackgroundColor,
+  handlePress,
+}: GoogleInputProps) => {
+  return <Text>GoogleTextInput</Text>;
+};
+
+export default GoogleTextInput;
